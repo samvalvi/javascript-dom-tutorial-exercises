1fd3517fcd2ec4f8b2cbd22fccf9c796364a8c53
samvalvi <samuelvalerin@gmail.com>
2021-02-24 18:20:46 +0000

    exercise 14

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,9 +1,19 @@
 // Your code here
-const deleteTask = () => {
+let addElement = document.getElementById("addToDo");
+let unsortedList = document.getElementsByTagName("UL");
+
+addElement.addEventListener('change', addTask);
+
+const addTask = (event) => {
+	let element = document.createElement("li");
+	element.innerHTML = event.target.value;
+
+	list.appendChild(element);
+};
+
+
+unsortedList.addEventListener('click', deleteTask);
+
+const deleteTask = (event) => {
     
-}
-
-
-const addTask = () => {
-
 }
