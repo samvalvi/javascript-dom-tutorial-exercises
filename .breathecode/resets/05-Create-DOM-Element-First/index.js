47da2236bfdc9de510bd6d7633b71a1e4733dc68
samvalvi <samuelvalerin@gmail.com>
2021-02-24 00:43:13 +0000

    exercise 8

--- .breathecode/resets/05-Create-DOM-Element-First/index.js
+++ .breathecode/resets/05-Create-DOM-Element-First/index.js
@@ -4,7 +4,3 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
-let element = document.createElement("p");
-element.innerHTML = "Hello World";
-element.style.backgroundColor = "yellow";
-document.querySelector("#myDiv").append(element);
